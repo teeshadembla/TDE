38ab17c442dd6cce93389b7bda58d302a409ae56
Teesha Dembla <teeshadembla0507@gmail.com>
2025-08-26 01:03:29 +0530

    resolving possible cors error

--- server/index.js
+++ server/index.js
@@ -22,8 +22,12 @@ import commentRouter from './Routes/commentRouter.js';
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
+    origin: process.env.NODE_ENV === 'production' 
+        ? [process.env.FRONTEND_URL, 'https://tde-kict.vercel.app'] 
+        : ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
