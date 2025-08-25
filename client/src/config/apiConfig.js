67a4fe8ab4634ddbb8f8037c877563a4862df7ea
Teesha Dembla <teeshadembla0507@gmail.com>
2025-08-26 00:39:10 +0530

    Changing api url

--- client/src/config/apiConfig.js
+++ client/src/config/apiConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = "http://localhost:8080";
+const API_URL = import.meta.env.PROD ? "": "http://localhost:8080";
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
