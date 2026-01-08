54d9d4809972708a9ada111f96f4f49323a27a64
Teesha Dembla <teeshadembla0507@gmail.com>
2026-01-08 18:57:00 +0530

    AWS credential error resolution attempt 1

--- server/utils/s3config.js
+++ server/utils/s3config.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION
+    region: process.env.AWS_REGION_PP
 });
 
 /**
