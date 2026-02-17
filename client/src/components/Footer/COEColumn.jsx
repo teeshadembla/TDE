bbc2eda0b5f7d831331505409775a69d3bc6ac23
Teesha Dembla <teeshadembla0507@gmail.com>
2026-02-17 17:36:04 +0530

    Change to footer to add 2 morepractice areas

--- client/src/components/Footer/COEColumn.jsx
+++ client/src/components/Footer/COEColumn.jsx
@@ -4,30 +4,43 @@ const COEColumn = () => {
             <h3 className="text-[rgb(173,173,173)] font-semibold  text-base text-[15.4px] mb-6">Center of Excellence</h3>
             <ul className="space-y-4">
               <li>
-                <a href="/digital-policy" className="text-white font-normal text-[14px] hover:text-white transition-colors duration-200 text-sm">
+                <a href="/ractice/698cdd95a0d9354807be9ba9" className="text-white font-normal text-[14px] hover:text-white transition-colors duration-200 text-sm">
                   Tech Policy and Governance
                 </a>
               </li>
               <li>
-                <a href="/blockchain-digital-assets" className="text-white font-normal text-[14px] hover:text-white transition-colors duration-200 text-sm">
+                <a href="/practice/698cdd95a0d9354807be9bae" className="text-white font-normal text-[14px] hover:text-white transition-colors duration-200 text-sm">
                   Digital Assets and BlockChain
                 </a>
               </li>
               <li>
-                <a href="sustainability" className="text-white font-normal text-[14px] hover:text-white transition-colors duration-200 text-sm">
+                <a href="practice/698cdd95a0d9354807be9bb3" className="text-white font-normal text-[14px] hover:text-white transition-colors duration-200 text-sm">
                   Sustainability in Tech
                 </a>
               </li>
               <li>
-                <a href="/applied-ai" className="text-white font-normal text-[14px] hover:text-white transition-colors duration-200 text-sm">
+                <a href="/practice/698cdd95a0d9354807be9bb8" className="text-white font-normal text-[14px] hover:text-white transition-colors duration-200 text-sm">
                   Applied Artificial Intelligence
                 </a>
               </li>
               <li>
-                <a href="/healthcare" className="text-white font-normal text-[14px] hover:text-white transition-colors duration-200 text-sm">
+                <a href="/practice/698cdd95a0d9354807be9bbd" className="text-white font-normal text-[14px] hover:text-white transition-colors duration-200 text-sm">
                   Healthcare Innovation
                 </a>
               </li>
+              
+              <li>
+                <a href="practice/698cdd95a0d9354807be9bc7" className="text-white font-normal text-[14px] hover:text-white transition-colors duration-200 text-sm">
+                  Cyber Studio
+                </a>
+              </li>
+              
+              <li>
+                <a href="practice/698cdd95a0d9354807be9bc2" className="text-white font-normal text-[14px] hover:text-white transition-colors duration-200 text-sm">
+                  Quantum Computing
+                </a>
+              </li>
+              
             </ul>
           </div>
     )
